501e7ea5b251f6291e2c15f65ac491bb9155e3ce
TonyNiTN <tony.zt.ni@gmail.com>
2023-02-08 23:40:51 -0500

    create new general error struct to handle error in codebase

--- internal/error/error.go
+++ internal/error/error.go
@@ -2,15 +2,15 @@ package error
 
 import "fmt"
 
-// Create base error class for sending requests
+// Create base error class for sending requests.
 type RequestError struct {
-	// HTTP status code
+	// HTTP status code.
 	StatusCode int
 
-	// Error message
+	// Error message.
 	Message string
 
-	// Type of HTTP request
+	// Type of HTTP request.
 	RequestType string
 }
 
@@ -25,5 +25,28 @@ func NewRequestError(requestType string, errorMessage string, statusCode int) *R
 		StatusCode:  statusCode,
 		Message:     errorMessage,
 		RequestType: requestType,
+	}
+}
+
+// Create base error for general error within codebase.
+type GeneralError struct {
+
+	// Function error appeared in.
+	Function string
+
+	// Error message.
+	Message string
+}
+
+// Create Error string.
+func (ge *GeneralError) Error() string {
+	return fmt.Sprintf("Error in Function: %s, Error Message: %s", ge.Function, ge.Message)
+}
+
+// Create a General Error struct.
+func NewGeneralError(function string, errorMessage string) *GeneralError {
+	return &GeneralError{
+		Function: function,
+		Message:  errorMessage,
 	}
 }
