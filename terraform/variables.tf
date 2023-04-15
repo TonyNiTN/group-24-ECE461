fd553b80db419e716e74deffea518891913e1de1
Ben Schwartz <schwartzb.2019@gmail.com>
2023-04-15 15:46:11 -0400

    Finish terraform (#9)
    * temp
    
    * add: api gateway
    
    * cloud logging only
    
    * api config
    
    * tf changes
    
    * tf working db, not working api config. boilerplate code for connecting to db in flask
    
    * ADD: SQL text files
    
    * ADD: sqlalchemy models
    
    * ADD: setup.py: create statements for Registry, Users, Ratings, and Binaries(?) Tables
    
    * addition of secrets for db passwords, service accounts, and other containers spinned up
    
    * ADD: SqlAlchemy code for packages
    
    * EDIT: Finish Registry SQLAlchemy select statement
    
    * ADD: SQL connect statement?
    
    * ADD: handle_packages
    
    * ADD: Ratings/Users structs
    
    * ADD: Handle Packages (all), with debug
    
    * swagger structure for go app
    
    * ADD: Handle_Package_ID mostly complete
    
    * separation of cloud run services and rendering api_spec with urls (test)
    
    * new db connect function for read apis
    
    * tf test with SQL and read container commented out
    
    * working api gw
    
    * update terraform files
    
    * comment out database for now
    
    * update api spec.yml
    
    * reorganize
    
    * add back in database
    
    * clean up unrelated code
    
    * api gw config gets correct service urls
    
    ---------
    
    Co-authored-by: Emile Baez <ebaezmunne@gmail.com>
    Co-authored-by: Ashassins <ashenassassinmc@gmail.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,23 @@
-variable "project_id" {
+variable "project_id"{
   description = "GCP Project ID"
   type        = string
+  sensitive   = false
+}
+
+variable "github_token" {
+  description = "Github API Token"
+  type        = string
   sensitive   = true
+}
+
+variable "read_user_password" {
+  description = "Read User Password"
+  type = string
+  sensitive = true
+}
+
+variable "write_user_password" {
+  description = "Write User Password"
+  type = string
+  sensitive = true
 }
