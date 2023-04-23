cb5af2b19ec1315a9c9a27d588118bd8307035c4
Ashassins <ashenassassinmc@gmail.com>
2023-04-23 04:04:56 -0400

    ADD: finally a working modal. + some JS skeleton code

--- docs/js/package.js
+++ docs/js/package.js
@@ -1,79 +1,140 @@
-const packageData = { 
-    id,
-    packagename,
-    url,
-    version,
-    author
-}
+// const packageData = { 
+//     id,
+//     packagename,
+//     url,
+//     version,
+//     author
+// }
 
-const packageRatings = {
-    ratingid,
-    busfactor,
-    correctness,
-    rampup,
-    license,
-    pinningpractice,
-    pullrequest,
-    netscore
-}
+// const packageRatings = {
+//     ratingid,
+//     busfactor,
+//     correctness,
+//     rampup,
+//     license,
+//     pinningpractice,
+//     pullrequest,
+//     netscore
+// }
 
-const packageDownload = {
-    downloadid,
-    downloadLink
-}
+// const packageDownload = {
+//     downloadId,
+//     downloadLink
+// }
 
-// name: 'John Doe',
+// var xhr = new XMLHttpRequest();
+// // global variables
+// {
+// var IDSlotEl = document.getElementById("IDSlot");
+// var NameSlotEl = document.getElementById("NameSlot");
+// var URLSlotEl = document.getElementById("URLSlot");
+// var VersionSlotEl = document.getElementById("VersionSlot");
+// var AuthorSlotEl = document.getElementById("AuthorSlot");
 
-fetch('/package/{id}', { // not 100% sure on this endpoint, might need more stuff
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(packageData)
-    })
-    .then(response => response.json())
+// var DownloadSlotEl = document.getElementById("DownloadSlot");
+// var DownloadIdSlotEl = document.getElementById("DownloadIdSlotEl");
 
-// fetch('/users', { // not 100% sure on this endpoint, might need more stuff
+// var RatingIDSlotEl = document.getElementById("RatingIDSlot");
+// var BusFactorSlotEl = document.getElementById("BusFactorSlot");
+// var CorrectnessSlotEl = document.getElementById("CorrectnessSlot");
+// var RampupSlotEl = document.getElementById("RampupSlot");
+// var LicenseSlotEl = document.getElementById("LicenseSlot");
+// var PPSlotEl = document.getElementById("PPSlot");
+// var PRSlotEl = document.getElementById("PRSlot");
+// var NetScoreSlotEl = document.getElementById("NetScoreSlot");
+// }
+
+// fetch('/package/:id', { // not 100% sure on this endpoint, might need more stuff
 //     method: 'POST',
 //     headers: {
 //     'Content-Type': 'application/json'
 //     },
-//     body: JSON.stringify(data)
+//     body: JSON.stringify(packageData)
 //     })
 //     .then(response => response.json())
 
-function setupPage() {
-    renderPackageInfo();
-    renderRatingInfo();
-}
+// // fetch('/users', { // not 100% sure on this endpoint, might need more stuff
+// //     method: 'POST',
+// //     headers: {
+// //     'Content-Type': 'application/json'
+// //     },
+// //     body: JSON.stringify(data)
+// //     })
+// //     .then(response => response.json())
 
-function renderPackageInfo() {
-    // HTML IDs: IDSlot, NameSlot, URLSlot, VersionSlot, AuthorSlot, DownloadSlot
-}
+// function setupPage() {
+//     renderPackageInfo();
+//     renderRatingInfo();
+// }
 
-function renderRatingInfo() {
-    // HTML IDs: RatingIDSlot, BusFactorSlot, CorrectnessSlot, RampupSlot, LicenseSlot, PPSlot, PRSlot, NetScoreSlot
-}
+// function renderPackageInfo() {
+//     // // HTML IDs: IDSlot, NameSlot, URLSlot, VersionSlot, AuthorSlot, DownloadSlot
+//     // IDSlotEl.value = packageData.id;
+//     // NameSlotEl.value = packageData.name;
+//     // URLSlotEl.value = packageData.url;
+//     // VersionSlotEl.value = packageData.version;
+//     // AuthorSlotEl.value = packageData.author;
+// }
 
-function checkPerms() {
-    
-}
+// function renderDownloadInfo() {
+//     // // DownloadIdSlotEl.value = packageDownload.downloadId; // wont be used
+//     // DownloadIdSlotEl.value = packageDownload.downloadLink;
+// }
 
-function openEditModal() {
-    // HTML IDs: packageEditModal
-    checkPerms();
-}
+// function renderRatingInfo() {
+//     // // HTML IDs: RatingIDSlot, BusFactorSlot, CorrectnessSlot, RampupSlot, LicenseSlot, PPSlot, PRSlot, NetScoreSlot
+//     // RatingIDSlotEl.value = packageRatings.ratingid;
+//     // BusFactorSlotEl.value = packageRatings.busfactor;
+//     // CorrectnessSlotEl.value = packageRatings.correctness;
+//     // RampupSlotEl.value = packageRatings.rampup;
+//     // LicenseSlotEl.value = packageRatings.license;
+//     // PPSlotEl.value = packageRatings.pinningpractice;
+//     // PRSlotEl.value = packageRatings.pullrequest;
+//     // NetScoreSlotEl.value = packageRatings.netscore;
+// }
 
-function closeEditModal() {
-    // HTML IDs: packageEditModal
-    setupPage();
-}
+// function openEditModal() {
+//     // // HTML IDs: packageEditModal
+//     checkPerms();
+// }
 
-function savePackage() {
-    // HTML IDs: IDSlotModal, NameSlotModal, URLSlotModal, VersionSlotModal, AuthorSlotModal
-    // HTML IDs: packageSaveModal
-}
+// function closeEditModal() {
+//     // // HTML IDs: packageEditModal
+//     setupPage();
+// }
 
-function deletePackage() {
-    // HTML IDs: packageSaveModal
-}
+// function savePackage() {
+//     // // HTML IDs: IDSlotModal, NameSlotModal, URLSlotModal, VersionSlotModal, AuthorSlotModal
+//     // // HTML IDs: packageSaveModal
+//     // IDSlotEl.value 
+//     // NameSlotEl.value 
+//     // URLSlotEl.value 
+//     // VersionSlotEl.value
+//     // AuthorSlotEl.value
+// }
+
+// function deletePackage() {
+//     // HTML IDs: packageSaveModal, packageDeleteModal
+//     xhr.open("DELETE", "/package/:id", true);
+//     xhr.setRequestHeader("Content-type", "application/json");
+//     xhr.onreadystatechange = function() {
+//         if (xhr.readyState == 4 && xhr.status == 200) { // status codes arent right yet
+//         // Handle success response
+//         } else {
+//         // Handle error response
+//         }
+//     }
+// }
+
+
+
+// // ----------------
+// // MODAL FUNCTIONS
+// // ----------------
+
+// // const packageModal = document.getElementById('packageEditModal')
+// // const packageInput = document.getElementById('myInput')
+
+// // myModal.addEventListener('shown.bs.modal', () => {
+// //     myInput.focus()
+// // })
